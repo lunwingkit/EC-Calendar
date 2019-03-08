58d73f575598241a9e3dd8bcac11da62974b1b9b
LUN WING KIT <lunwingkit@gmail.com>
2019-03-08 22:50:56 +0800

    Init
    Init

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by LUN WING KIT on 8/3/2019.
+//  Copyright © 2019 LUN WING KIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
