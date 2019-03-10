477bd619a50f67fda27a3ae1d9b07a12634735be
LUN WING KIT <lunwingkit@gmail.com>
2019-03-11 00:58:35 +0800

    ui

---
+++ Project1/Project1/CalendarViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CalendarViewController.swift
+//  Project1
+//
+//  Created by LUN WING KIT on 10/3/2019.
+//  Copyright © 2019 LUN WING KIT. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    var yearHeader : UIView!
+    var yearLabel : UILabel!
+    
+    var weekview: UIView!
+    var weekStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        //navigationController?.navigationBar.barTintColor = .darkGray
+        //navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.title = "Calendar"
+        
+        
+        let calendar = EC_Calendar(year: 2018);
+        calendar.printYear();
+        
+        configureHeader()
+    }
+    
+    func configureHeader(){
+        yearHeader = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50))
+        yearHeader.backgroundColor = .darkGray
+        self.view.addSubview(yearHeader)
+        
+        yearHeader.translatesAutoresizingMaskIntoConstraints = false
+        
+        //yearHeader.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        //yearHeader.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        //yearHeader.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        //yearHeader.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        
+        
+        yearLabel = UILabel(frame: yearHeader.frame)
+        yearLabel.text = "YEAR"
+        yearLabel.backgroundColor = .white
+        yearLabel.textAlignment = .center
+        yearLabel.font = UIFont(name: yearLabel.font.fontName, size: 20)
+        yearHeader.addSubview(yearLabel)
+        
+        
+        
+        weekview = UIView(frame: CGRect(x: 0, y: 50, width: self.view.frame.width, height: 50))
+        
+        weekStackView = UIStackView(frame: self.view.frame)
+        weekStackView.axis = .horizontal
+        weekStackView.alignment = .fill
+        weekStackView.distribution = .fill
+        weekStackView.backgroundColor = .red
+        let label = UILabel()
+        label.text = "Text"
+        label.backgroundColor = .yellow
+        weekStackView.addArrangedSubview(label)
+        self.view.addSubview(weekStackView)
+    }
+}
---
+++ Project1/Project1/ContainerController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by LUN WING KIT on 8/3/2019.
+//  Copyright © 2019 LUN WING KIT. All rights reserved.
+//
+
+import UIKit
+
+class ContainerController: UIViewController {
+    
+    var displayController: UIViewController!
+    
+    var calendarViewController: CalendarViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .white
+        //navigationItem.title = "Calendar"
+        configureCalendarViewController()
+    }
+    
+    func configureCalendarViewController(){
+        calendarViewController = CalendarViewController()
+        displayController = UINavigationController(rootViewController: calendarViewController)
+        
+        view.addSubview(displayController.view)
+        addChild(displayController)
+        displayController.didMove(toParent: self)
+        
+    }
+    
+    
+}
+
+
