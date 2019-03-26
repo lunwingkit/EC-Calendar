4ec87492e74230364fefa09c371f0ab2d7106465
LUN WING KIT <lunwingkit@gmail.com>
2019-03-27 01:49:37 +0800

    added Eureka for form handling
    Eureka

---
+++ Project1/Project1/Calendar/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  Project1
+//
+//  Created by LUN WING KIT on 26/3/2019.
+//  Copyright © 2019 LUN WING KIT. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell, UITextFieldDelegate {
+
+    //@IBOutlet weak var textField: UITextFiled!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+class TextTableViewCell: CustomTableViewCell{
+    //@IBOutlet weak var
+}
